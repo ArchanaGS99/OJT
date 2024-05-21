a4ba4b250c001ed474b0055f345175b33a55ebbc
Archana <archanaarcha99@gmail.com>
2024-05-21 15:40:54 +0530

    4th commit

--- 21.05.2024/q2.py
+++ 21.05.2024/q2.py
@@ -1,20 +1,18 @@
-class Animal:
-    def speak(self):
-        raise NotImplementedError("Subclass must implement abstract method")
+#Bus Class Inheriting from Vehicle Class
 
-class Dog(Animal):
-    def speak(self):
-        return "Woof!"
+class Vehicle:
+    def __init__(self, name, seating_capacity):
+        self.name = name
+        self.seating_capacity = seating_capacity
 
-class Cat(Animal):
-    def speak(self):
-        return "Meow!"
+    def fare(self):
+        return self.seating_capacity * 100
 
-class Cow(Animal):
-    def speak(self):
-        return "Moo!"
+class Bus(Vehicle):
+    def fare(self):
+        base_fare = super().fare()
+        total_fare = base_fare + (0.1 * base_fare)
+        return total_fare
 
-# Demonstrating polymorphism
-animals = [Dog(), Cat(), Cow()]
-for animal in animals:
-    print(animal.speak())
+bus = Bus("City Bus", 50)
+print(f"Total Bus Fare: {bus.fare()}")
